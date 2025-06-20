925618e375fd58beed9dfd57edd41c879b295c7e
work <work.temp971@gmail.com>
2025-06-20 23:16:56 +0530

    version 0.0.2

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/space/',
+  base: '/spadecode/',
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
